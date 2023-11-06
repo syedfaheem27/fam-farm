3737253c02285547f5af17f7b2dda387b6679882
syedfaheem27 <syedfaheem2719@gmail.com>
2023-11-06 13:06:17 +0530

    Add slugify,nodemon and create slugs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-const replaceTemplate = require("./modules/replaceTemplate");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const slugify = require('slugify');
+
+const replaceTemplate = require('./modules/replaceTemplate');
 //FILE Reading and Writing
 
 //Synchronous file reading and writing
@@ -34,58 +37,60 @@ const replaceTemplate = require("./modules/replaceTemplate");
 
 //Reading data synchronously
 //No point in reading the same data over and over again
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(data => slugify(data.productName, { lower: true }));
 
 const overviewTemplate = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 
 const cardTemplate = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 
 const productTemplate = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
 
-  if (pathname === "/" || pathname === "/overview") {
+  if (pathname === '/' || pathname === '/overview') {
     const output = dataObj
-      .map((data) => {
+      .map(data => {
         return replaceTemplate(cardTemplate, data);
       })
-      .join("");
-    const overview = overviewTemplate.replace("{%PRODUCT_CARDS%}", output);
+      .join('');
+    const overview = overviewTemplate.replace('{%PRODUCT_CARDS%}', output);
     res.writeHead(200, {
-      "Content-Type": "text/html",
+      'Content-Type': 'text/html',
     });
     res.end(overview);
-  } else if (pathname === "/products") {
+  } else if (pathname === '/products') {
     const product = dataObj[query.id];
     const output = replaceTemplate(productTemplate, product);
     res.writeHead(200, {
-      "Content-Type": "text/html",
+      'Content-Type': 'text/html',
     });
     res.end(output);
-  } else if (pathname === "/api") {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     });
     res.end(data);
   } else {
     res.writeHead(404, {
-      "Content-Type": "text/html",
+      'Content-Type': 'text/html',
     });
-    res.end("<h1>Page Not Found</h1>");
+    res.end('<h1>Page Not Found</h1>');
   }
 });
 
-server.listen(8001, "127.0.0.1", () => {
-  console.log("Server running on port 8001");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server running on port 8000');
 });
